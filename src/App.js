fbb68bcdc76dc3a62aee153ee35f8f11395df487
u8756809 <u8756809@bsmch.net>
2022-04-03 11:01:08 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "./components/Chart";
+import TeamName from "./components/TeamName";
+
+const ldata = [
+  { label: "Team 301", y: 0 },
+  { label: "Team 302", y: 0 },
+  { label: "Team 303", y: 0 },
+  { label: "Team 304", y: 0 },
+  { label: "Team 305", y: 0 },
+  { label: "Team 306", y: 0 },
+  { label: "Team 307", y: 0 },
+  { label: "Team 308", y: 0},
+  { label: "Team 309", y: 0 },
+  { label: "Team 310", y: 0 },
+];
+
+function App() {
+  const MINUTE_MS = 10000;
+  const [data, setData] = useState(ldata);
+  const prevDataRef = useRef(data);
+  const [tallestHeights, setTallestHeights] = useState([[], [], []]);
+
+  useEffect(() => {
+    prevDataRef.current = ldata;
+    setData(() => {
+      return [...prevDataRef.current];
+    });
+  }, [ldata]);
+
+  useEffect(() => {
+    const heightsArray = data.map((item) => item.y);
+    let firstPlaces = [[], [], []];
+    for (let index = 0; index <= 10; index++) {
+      const heighest = Math.max(...heightsArray);
+      let sliceAtIndex = heightsArray.indexOf(heighest);
+      if (heighest === firstPlaces[0][0] || firstPlaces[0].length === 0) {
+        firstPlaces[0].push(...heightsArray.splice(sliceAtIndex, 1));
+      } else if (
+        heighest === firstPlaces[1][0] ||
+        firstPlaces[1].length === 0
+      ) {
+        firstPlaces[1].push(...heightsArray.splice(sliceAtIndex, 1));
+      } else if (
+        heighest === firstPlaces[2][0] ||
+        firstPlaces[2].length === 0
+      ) {
+        firstPlaces[2].push(...heightsArray.splice(sliceAtIndex, 1));
+      }
+    }
+    setTallestHeights(firstPlaces);
+  }, [data, ldata]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let rdata = data;
+      fetch("http://52.224.2.235:8008/shares")
+        .then((response) => response.json())
+        .then((data) => {
+          data.forEach((element) => {
+            try {
+              rdata[element.id - 1].label = element.company_id;
+              rdata[element.id - 1].y = parseInt(element.score);
+            } catch {
+              console.log(element);
+            }
+          });
+          setData(rdata);
+        });
+    }, MINUTE_MS);
+  }, [data]);
+
+  return (
+    <div className="app-container">
+      <header className="title-container">
+        <img
+          id="logo"
+          src={process.env.PUBLIC_URL + "assests/CytakaLogo-removebg.png"}
+        />
+        <h1 className="title">{"Cytaka Cyber Championship"}</h1>
+        <div className="title-background"></div>
+      </header>
+      <div className="glow">
+        {data.map((chartData, index) => (
+          <Chart
+            key={index}
+            index={index}
+            chartData={chartData}
+            data={data}
+            tallestHeights={tallestHeights}
+          />
+        ))}
+      </div>
+      <div className="chart-container">
+        {data.map((chartData, index) => (
+          <div className="chartgrid" key={index}>
+            <Chart
+              index={index}
+              chartData={chartData}
+              data={data}
+              tallestHeights={tallestHeights}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="teamname-container">
+        {data.map((chartData, index) => (
+          <div className="chartgrid" key={index}>
+            <TeamName data={data} chartData={chartData} />
+          </div>
+        ))}
+      </div>
+      <div className="chart-container-3"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chart.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState, useRef} from "react";
+
+const Chart = ({ chartData, tallestHeights }) => {
+  const currentChartHeight = chartData.y;
+  const [gredientArray, setGredientArray] = useState([]);
+  const [textColor, setTextColor] = useState();
+
+  const setGredient = () => {
+    if (tallestHeights[0][0] === currentChartHeight && tallestHeights[0][0] !== 0) {
+      setGredientArray(["#8E3426", "#FFA500", "#FFFF00"]);
+      setTextColor("#FFFF00");
+    } else if (tallestHeights[1][0] === currentChartHeight) {
+      setGredientArray(["#281356", "#005BE7", "#00FFFF"]);
+      setTextColor("#00FFFF");
+    } else if (tallestHeights[2][0] === currentChartHeight) {
+      setTextColor("rgb(250,150,120)");
+      setGredientArray(["rgb(120,10,30)", "#FF0000", "#E03800"]);
+    } else {
+      setGredientArray([
+        "rgba(255,255,255,0.1)",
+        "rgba(255,255,255,0.1)",
+        "rgba(255,255,255,0.1)",
+      ]);
+      setTextColor("rgba(255,255,255,0.2)");
+    }
+  };
+
+  useEffect(() => {
+    setGredient();
+  }, [tallestHeights]);
+
+  const chartStyle = {
+    width: "auto",
+    height: `${
+      tallestHeights[0][0] && (currentChartHeight / tallestHeights[0][0] <= 0.05) ? "2" : (currentChartHeight / tallestHeights[0][0]) * 100
+    }%`,
+    backgroundImage: `linear-gradient(15deg, ${gredientArray[0]} 20%, ${gredientArray[1]} 78%, ${gredientArray[2]} 110%)`,
+    transition: "height 3s ease-out",
+  };
+
+  return (
+    <div className="chart-column">
+      <p className="score" style={{ color: textColor }}>
+        {chartData.y}
+      </p>
+      <div className="rectangle" style={{ ...chartStyle }}></div>
+    </div>
+  );
+};
+
+export default Chart;
